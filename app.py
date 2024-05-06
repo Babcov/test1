6d2e1b21ab9c67de2ccf48640df600256163f50a
Babkow <babkow_av.jur@mail.ru>
2024-05-06 17:36:31 +0300

    ok13

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+news_articles = [
+    {
+        'title': 'Новость 1',
+        'content': 'Содержание новости 1'
+    },
+    {
+        'title': 'Новость 2',
+        'content': 'Содержание новости 2'
+    },
+    # Добавьте больше новостей
+]
+
+# Главная страница
+@app.route('/')
+def index():
+    return render_template('index.html', news_articles=news_articles)
+
+# Это нужно добавить
+@app.route('/index')
+def index_redirect():
+    return index()
+
+if __name__ == '__main__':
+    app.run(debug=True)
