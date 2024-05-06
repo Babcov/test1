c77f048dba72e2ce26c40ccda34c33b4a0b21ee7
Babkow <babkow_av.jur@mail.ru>
2024-05-06 21:10:10 +0300

    1

--- news/app.py
+++ news/app.py
@@ -1,28 +1,29 @@
-from flask import Flask 
-from waitress import serve
+from flask import Flask, render_template
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.jinja_env.auto_reload = True
 
-app.config['TEMPLATES_AUTO_RELOAD'] = True
-app.jinja_env.auto_reload = True
+    news_articles = [
+        {
+            'title': 'Новость 1',
+            'content': 'Содержание новости 1'
+        },
+        {
+            'title': 'Новость 2',
+            'content': 'Содержание новости 2'
+        },
+    ]
 
-news_articles = [
-    {
-        'title': 'Новость 1',
-        'content': 'Содержание новости 1'
-    },
-    {
-        'title': 'Новость 2',
-        'content': 'Содержание новости 2'
-    },
-    # Добавьте больше новостей
-]
+    @app.route('/')
+    def index():
+        return render_template('index.html', news_articles=news_articles)
 
-# Главная страница
-@app.route('/')
-def index():
-    return render_template('index.html', news_articles=news_articles)
+    return app
+
+app = create_app()
 
 if __name__ == '__main__':
-   from waitress import serve
-serve(app, host='0.0.0.0', port=8080)
+    from waitress import serve
+    serve(app, host='0.0.0.0', port=8080)
