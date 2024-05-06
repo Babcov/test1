a66790ce17d4ffd863b6d98082590d17726e1bba
Babkow <babkow_av.jur@mail.ru>
2024-05-06 20:58:44 +0300

    1

--- news/app.py
+++ news/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask 
+from waitress import serve
 
 app = Flask(__name__)
 
@@ -23,5 +24,5 @@ def index():
     return render_template('index.html', news_articles=news_articles)
 
 if __name__ == '__main__':
-    from waitress import serve
-    serve(app, host='0.0.0.0', port=8080)
+   from waitress import serve
+serve(app, host='0.0.0.0', port=8080)
