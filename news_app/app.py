47569f9fe2e3acb50adf6d677a1e8ac928d5cc8d
Babkow <babkow_av.jur@mail.ru>
2024-05-06 12:42:45 +0300

    first commit

---
+++ news_app/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+news_articles = [
+    {
+        'title': 'Новость 1',
+        'content': 'Содержание новости 1'
+    },
+    {
+        'title': 'Новость 2',
+        'content': 'Содержание новости 2'
+    },
+    # Добавьте больше новостей
+]
+
+@app.route('/')
+def index():
+    return render_template('index.html', news_articles=news_articles)
+
+if __name__ == '__main__':
+    app.run(debug=True)
