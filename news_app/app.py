60a9d72008003df983b397d2e4e8870105e49682
Babkow <babkow_av.jur@mail.ru>
2024-05-06 17:42:50 +0300

    ok15

--- news_app/app.py
+++ news_app/app.py
@@ -19,10 +19,5 @@ news_articles = [
 def index():
     return render_template('index.html', news_articles=news_articles)
 
-# Это нужно добавить
-@app.route('/index')
-def index_redirect():
-    return index()
-
 if __name__ == '__main__':
     app.run(debug=True)
